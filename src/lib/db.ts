42819bef892899900d28463b170be2580ad04021
gitAarya <aaryagithub@gmail.com>
2025-06-15 21:35:01 +0545

    Add MongoDB connection and user/video models with bcrypt integration

---
+++ src/lib/db.ts
@@ -0,0 +1,39 @@
+import { channel } from "diagnostics_channel";
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL!;
+if(!MONGODB_URL){
+    throw new Error("please define mongoDB url is env file")
+}
+
+let cached = global.mongoose;
+if(!cached){
+    cached=global.mongoose={conn:null,promise:null}
+
+}
+
+export async function connToDB() {
+    if(cached.conn){
+         return cached.conn
+    }
+
+    if(!cached.promise){
+        const opts={
+            bufferCommands:true,
+            MaxPoolSize:10,
+        }
+        cached.promise = mongoose.connect(MONGODB_URL, opts).then( ()=> mongoose.connection)
+
+
+    }
+
+    try {
+        cached.conn=await cached.promise
+        
+    } catch (error) {
+        cached.promise=null
+        throw new Error(" db promise error")
+    }
+
+    return cached.conn;
+}
---
+++ src/models/video.ts
@@ -0,0 +1,64 @@
+import mongoose, { Schema, model, models } from "mongoose";
+import bcrypt from "bcryptjs";
+import { title } from "process";
+import { types } from "util";
+
+export const VideoDimension={
+    width:1080,
+    height:1920
+}
+export interface IVIDEO {
+  _id?: mongoose.Types.ObjectId;
+  title: string;
+  description: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  controls?: boolean;
+  transfromation?: {
+    height: number;
+    width: number;
+    quality?: number;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+const VideoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  videoUrl: {
+    type: String,
+    required: true,
+  },
+  thumbnailUrl: {
+    type: String,
+    required: true,
+  },
+  controls: {
+    type: Boolean,
+    default: true,
+  },
+  transfromation: {
+    height: {
+      type: Number,
+      default: VideoDimension.height,
+    },
+    width: {
+      type: Number,
+      default: VideoDimension.width,
+    },
+    quality:{
+        type:Number,
+        min:1,
+        max:100
+    }
+  },
+},
+{
+    timestamps:true
+});
