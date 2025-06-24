7527a28918f840892bdbe799df07d2963974ca44
gitAarya <aaryagithub@gmail.com>
2025-06-24 22:26:29 +0545

    Refactor authentication logic and improve error handling in NextAuth integration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,4 @@
-import CredentialsProvider from "next-auth/providers/credentials";
-import { connToDB } from "./db";
-import User from "@/models/user.model";
-import bcrypt from "bcryptjs";
-
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import { authOptions } from "@/lib/auth";
-export const authOptions: NextAuthOptions = {
-
-  callbacks: {
-    async jwt(token, user) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.id as string;
-      }
-      return session;
-    },
-  },
-  session: {
-    strategy: "jwt",
-  },
-};
-const handler= NextAuth(authOptions)
+const handler= NextAuth(authOptions);
 export {handler as GET,handler as POST}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,42 +1,58 @@
 import { NextAuthOptions } from "next-auth";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import  CredentialsProvider  from "next-auth/providers/credentials";
 import { connToDB } from "./db";
 import bcrypt from "bcryptjs";
 import User from "@/models/user.model";
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      name: "Credentials",
+      name: "credentials",
       credentials: {
         email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials: { email: any; password: string; },req: any) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Missing Email or Password");
+      async authorize(credentials) {
+        if (!credentials?.email || credentials?.password) {
+          throw new Error(" email or password is missing");
         }
         try {
           await connToDB();
+
           const user = await User.findOne({ email: credentials.email });
           if (!user) {
-            throw new Error("no user found");
+            throw new Error("no user found!");
           }
-
-          const isvalid = await bcrypt.compare(
+          const isValid = await bcrypt.compare(
             credentials.password,
             user.password
           );
-          if (!isvalid) {
-            throw new Error("Invalid Password");
+          if (!isValid) {
+            throw new Error("invalid password");
           }
           return {
             id: user._id.toString(),
             email: user.email,
           };
         } catch (error) {
+          console.log("auth error", error);
+
           throw error;
         }
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+       session.user.id=token.id as string
+      }
+      return session;
+    },
+  },
 };
